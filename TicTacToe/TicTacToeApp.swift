c1b963eef38c368d52cc4a8c83b5cd5006241c47
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2024-03-29 11:07:08 -0400

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Christopher Yoon on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
